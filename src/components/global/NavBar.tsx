fbee0f6749c7165e6afd0429ab7081d2442711eb
Apluri <henri.hietala95@gmail.com>
2022-06-09 13:27:09 +0300

    add menu to navbar to reduce space for responsiveness

--- src/components/global/NavBar.tsx
+++ src/components/global/NavBar.tsx
@@ -6,7 +6,7 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
-import { Avatar } from "@mui/material";
+import { Avatar, Menu, MenuItem } from "@mui/material";
 import { useAuth } from "../providers/AuthContext";
 import { ProfileInfo } from "../users/ProfileInfo";
 import cuteLogo from "../../assets/images/cute-logo.jpg";
@@ -15,8 +15,18 @@ import { useNavigate } from "react-router-dom";
 export default function NavBar() {
   const auth = useAuth();
   const navigate = useNavigate();
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box sx={{}}>
       <AppBar position="static" color="secondary">
         <Toolbar sx={{ justifyContent: "space-between" }}>
           <Box sx={{ flex: 1, display: "flex", justifyContent: "flex-start" }}>
@@ -50,14 +60,33 @@ export default function NavBar() {
             {auth?.currentUser ? (
               <>
                 {auth.currentUser.isAnonymous ? (
-                  <Typography sx={{ display: "flex", alignSelf: "center" }}>
+                  <Typography
+                    onClick={handleClick}
+                    sx={{ display: "flex", alignSelf: "center" }}
+                  >
                     Anonymous user
                   </Typography>
                 ) : (
-                  <ProfileInfo />
+                  <ProfileInfo onClick={handleClick} />
                 )}
-
-                <Button onClick={() => auth?.logOut()}>Log Out</Button>
+                <Menu open={open} onClose={handleClose} anchorEl={anchorEl}>
+                  <MenuItem
+                    onClick={() => {
+                      navigate("/profile");
+                      handleClose();
+                    }}
+                  >
+                    Profile
+                  </MenuItem>
+                  <MenuItem
+                    onClick={() => {
+                      auth?.logOut();
+                      handleClose();
+                    }}
+                  >
+                    Log out
+                  </MenuItem>
+                </Menu>
               </>
             ) : (
               <Button color="inherit" onClick={() => auth?.signIn()}>
--- src/components/users/ProfileInfo.tsx
+++ src/components/users/ProfileInfo.tsx
@@ -2,11 +2,17 @@ import React from "react";
 import { Avatar, Box, Button, Typography } from "@mui/material";
 import { useDatabase } from "../providers/DatabaseContext";
 
-export function ProfileInfo() {
+type Props = {
+  onClick?: (event: any) => void;
+};
+export function ProfileInfo({ onClick }: Props) {
   const db = useDatabase();
   const username = db?.user?.userName;
   return (
-    <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+    <Box
+      onClick={(e) => onClick && onClick(e)}
+      sx={{ display: "flex", flexDirection: "row", alignItems: "center" }}
+    >
       <Typography variant="h6">{username && username}</Typography>
     </Box>
   );
