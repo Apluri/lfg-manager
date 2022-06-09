bbebd38f4ec1a3e4ea3b23d669eb122a05a790d3
Apluri <henri.hietala95@gmail.com>
2022-06-09 13:39:39 +0300

    add responsive textsizes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,7 +5,11 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./screens/Home";
 import NavBar from "./global/NavBar";
 import { DatabaseProvider } from "./providers/DatabaseContext";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
+import {
+  ThemeProvider,
+  createTheme,
+  responsiveFontSizes,
+} from "@mui/material/styles";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterLuxon } from "@mui/x-date-pickers/AdapterLuxon";
 import { AdminPanel } from "./screens/AdminPanel";
@@ -14,11 +18,14 @@ import { LfgScreen } from "./screens/LfgScreen";
 import { Box } from "@mui/system";
 import { Router } from "./screens/Router";
 
-const darkTheme = createTheme({
-  palette: {
-    mode: "dark",
-  },
-});
+const darkTheme = responsiveFontSizes(
+  createTheme({
+    palette: {
+      mode: "dark",
+    },
+  })
+);
+
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
