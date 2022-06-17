abcf402ccdd27f4cbb234a92362689fb5b1c798c
Apluri <henri.hietala95@gmail.com>
2022-06-17 11:15:40 +0300

    Hide navbar when scrolling down

--- src/components/global/NavBar.tsx
+++ src/components/global/NavBar.tsx
@@ -7,7 +7,7 @@ import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import MenuIcon from "@mui/icons-material/Menu";
-import { Avatar, Menu, MenuItem } from "@mui/material";
+import { Avatar, Menu, MenuItem, Slide, useScrollTrigger } from "@mui/material";
 import { useAuth } from "../providers/AuthContext";
 import { ProfileInfo } from "../users/ProfileInfo";
 import cuteLogo from "../../assets/images/cute-logo.jpg";
@@ -57,78 +57,96 @@ export default function NavBar({ setSideBarMargin }: Props) {
     }
   };
 
+  type ScrollProps = {
+    children: React.ReactElement;
+  };
+  function HideOnScroll({ children }: ScrollProps) {
+    const trigger = useScrollTrigger();
+    return (
+      <Slide appear={false} direction="down" in={!trigger}>
+        {children}
+      </Slide>
+    );
+  }
   return (
-    <AppBar position="sticky" color="secondary">
-      <Toolbar sx={{ justifyContent: "space-between" }}>
-        <Box sx={{ flex: 1, display: "flex", justifyContent: "flex-start" }}>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-            onClick={openSideBar}
-          >
-            <MenuIcon />
-          </IconButton>
-        </Box>
-        <Sidebar
-          width={sideBarWidth}
-          setWidth={(num) => {
-            setSideBarWidth(num);
-            sideBarRef.current = num;
-          }}
-          setSideBarMargin={setSideBarMargin}
-        />
-        <Box className="appTitleContainer" onClick={() => navigate("/")}>
-          <Typography variant="h4" sx={{ paddingRight: "10px" }}>
-            Cute guild
-          </Typography>
-          <Avatar src={cuteLogo} sx={{ alignSelf: "center" }} />
-        </Box>
-
-        <Box className="profileBtn">
-          {auth?.currentUser ? (
-            <>
-              {auth.currentUser.isAnonymous ? (
-                <Typography
-                  onClick={handleClick}
-                  sx={{ display: "flex", alignSelf: "center" }}
-                >
-                  Anonymous user
-                </Typography>
-              ) : (
-                <ProfileInfo onClick={handleClick} />
-              )}
-              <IconButton onClick={handleClick}>
-                <ArrowDropDownIcon />
+    <>
+      <HideOnScroll>
+        <AppBar position="sticky" color="secondary" sx={{ zIndex: 1 }}>
+          <Toolbar sx={{ justifyContent: "space-between" }}>
+            <Box
+              sx={{ flex: 1, display: "flex", justifyContent: "flex-start" }}
+            >
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+                onClick={openSideBar}
+              >
+                <MenuIcon />
               </IconButton>
-              <Menu open={open} onClose={handleClose} anchorEl={anchorEl}>
-                <MenuItem
-                  onClick={() => {
-                    navigate("/profile");
-                    handleClose();
-                  }}
-                >
-                  Profile
-                </MenuItem>
-                <MenuItem
-                  onClick={() => {
-                    auth?.logOut();
-                    handleClose();
-                  }}
-                >
-                  Log out
-                </MenuItem>
-              </Menu>
-            </>
-          ) : (
-            <Button color="inherit" onClick={() => auth?.signIn()}>
-              Login
-            </Button>
-          )}
-        </Box>
-      </Toolbar>
-    </AppBar>
+            </Box>
+
+            <Box className="appTitleContainer" onClick={() => navigate("/")}>
+              <Typography variant="h4" sx={{ paddingRight: "10px" }}>
+                Cute guild
+              </Typography>
+              <Avatar src={cuteLogo} sx={{ alignSelf: "center" }} />
+            </Box>
+
+            <Box className="profileBtn">
+              {auth?.currentUser ? (
+                <>
+                  {auth.currentUser.isAnonymous ? (
+                    <Typography
+                      onClick={handleClick}
+                      sx={{ display: "flex", alignSelf: "center" }}
+                    >
+                      Anonymous user
+                    </Typography>
+                  ) : (
+                    <ProfileInfo onClick={handleClick} />
+                  )}
+                  <IconButton onClick={handleClick}>
+                    <ArrowDropDownIcon />
+                  </IconButton>
+                  <Menu open={open} onClose={handleClose} anchorEl={anchorEl}>
+                    <MenuItem
+                      onClick={() => {
+                        navigate("/profile");
+                        handleClose();
+                      }}
+                    >
+                      Profile
+                    </MenuItem>
+                    <MenuItem
+                      onClick={() => {
+                        auth?.logOut();
+                        handleClose();
+                      }}
+                    >
+                      Log out
+                    </MenuItem>
+                  </Menu>
+                </>
+              ) : (
+                <Button color="inherit" onClick={() => auth?.signIn()}>
+                  Login
+                </Button>
+              )}
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
+      <Sidebar
+        width={sideBarWidth}
+        setWidth={(num) => {
+          setSideBarWidth(num);
+          sideBarRef.current = num;
+        }}
+        setSideBarMargin={setSideBarMargin}
+      />
+    </>
   );
 }
