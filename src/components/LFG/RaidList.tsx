0e8ffed5fd0505d51c6e24ccf8a63f19529c485b
Apluri <henri.hietala95@gmail.com>
2022-06-20 13:15:29 +0300

    shrink lfg post, toggle open, add summary info

--- src/components/LFG/RaidList.tsx
+++ src/components/LFG/RaidList.tsx
@@ -1,10 +1,36 @@
-import { Paper, Typography } from "@mui/material";
+import {
+  Accordion,
+  AccordionSummary,
+  Collapse,
+  Paper,
+  Typography,
+} from "@mui/material";
 import { Box } from "@mui/system";
 import React, { useState } from "react";
 import { Character } from "../../utils/CharacterUtils";
 import { ApplicantCard } from "./ApplicantCard";
 import { EmptySlot } from "./EmptySlot";
 import { Applicant, LfgPost } from "./LfgPosts";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import IconButton, { IconButtonProps } from "@mui/material/IconButton";
+import { styled } from "@mui/material/styles";
+import { useDatabase } from "../providers/DatabaseContext";
+import { useAuth } from "../providers/AuthContext";
+import PersonOffOutlinedIcon from "@mui/icons-material/PersonOffOutlined";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+
+interface ExpandMoreProps extends IconButtonProps {
+  expand: boolean;
+}
+const ExpandMore = styled((props: ExpandMoreProps) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
 
 const PARTY_SIZE = 4;
 type Props = {
@@ -19,6 +45,8 @@ export function RaidList({
   handleLeaveRaid,
   post,
 }: Props) {
+  const [open, setOpen] = useState(false);
+  const auth = useAuth();
   function displaySlots(startIndex: number, endIndex: number) {
     let raidSlots: JSX.Element[] = [];
     for (let i = startIndex; i < raidSize && i < endIndex; i++) {
@@ -37,5 +65,49 @@ export function RaidList({
     }
     return <>{raidSlots}</>;
   }
-  return <Box className="applicantContainer">{displaySlots(0, raidSize)}</Box>;
+  function toggleOpen() {
+    setOpen(!open);
+  }
+  function isUserInRaid(): boolean {
+    return applicants?.find(
+      (applicant) => applicant.uid === auth?.currentUser?.uid
+    )
+      ? true
+      : false;
+  }
+  return (
+    <Box>
+      <Box
+        onClick={toggleOpen}
+        sx={{
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        {isUserInRaid() ? (
+          <PersonOutlineOutlinedIcon color="success" />
+        ) : (
+          <PersonOffOutlinedIcon color="disabled" />
+        )}
+
+        <Typography>
+          {applicants?.length ?? 0} / {raidSize}
+        </Typography>
+        <ExpandMore
+          expand={open}
+          onClick={toggleOpen}
+          aria-expanded={open}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </Box>
+
+      <Collapse in={open}>
+        <Box className="applicantContainer">{displaySlots(0, raidSize)}</Box>
+      </Collapse>
+    </Box>
+  );
 }
